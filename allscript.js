3b76436ccdd1062a75ac7aef6c65c7bcaf60c89c
burakkarakaya <burakkarakaya1984@gmail.com>
2015-06-29 10:22:05 +0300

    allascript update

--- allscript.js
+++ allscript.js
@@ -1,19 +1,31 @@
 /////////////////////////////////////////////////////////////////////////////////////////////////////// GLOBAL VARIABLE
 var win = $( window ), doc = $( document ), bdy = $('body'), pages = { main: '.pageHome', list: '.pageList', detail: '.pageDetail', blog: '.pageBlog' }, wt,  ht, wst, sRatio = 0, rstDom = false;
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////// 
-if( $( pages['main'] ).length > 0 ){
-	// noThing
+/////////////////////////////////////////////////////////////////////////////////////////////////////// MAIN PAGE
+function mainPage(){
+	//noThing
 }
-if( $( pages['list'] ).length > 0 ){
-	// noThing
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////// LIST PAGE
+function listPage(){
+	//noThing
 }
-if( $( pages['detail'] ).length > 0 ){
-	// noThing
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////// DETAIL PAGE
+function detailPage(){
+	//noThing
 }
-if( $( pages['blog'] ).length > 0 ){
-	// noThing
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////// BLOG PAGE
+function blogPage(){
+	//noThing
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////// PAGES
+if( detectEl( $( pages['main'] ) ) ) mainPage();
+if( detectEl( $( pages['list'] ) ) ) listPage();
+if( detectEl( $( pages['detail'] ) ) ) detailPage();
+if( detectEl( $( pages['blog'] ) ) ) blogPage();
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////// GLOBAL FUNC
 
@@ -34,9 +46,11 @@ function getAjax( uri, callback, error ){
 	});
 }
 
+function detectEl( ID ){ return ID.length > 0 ? true : false; }
+
 function cssClass( o, callback ){
 	var ID = $( o['ID'] ), k = o['delay'], type = o['type'], cls;
-	if( ID.length > 0 ){
+	if( detectEl( ID ) ){
 		if( type == 'add' ){
 			cls = o['cls'] || ['ready', 'animate'];
 			ID.addClass( cls[ 0 ] ).delay( k ).queue('fx', function(){ $( this ).dequeue().addClass( cls[ 1 ] ); if( callback != undefined ) callback(); });
@@ -52,22 +66,22 @@ function pageScroll( t, callback ){
 }
 
 function triggerMinusScroller( ID ){
-	if( ID.length > 0 ){
+	if( detectEl( ID ) ){
 		ID.minusScroller({ resize:true, navigation:true, snap: true, padding:0, divName: { wrapper: '#ajxUrunList > .prdListHolder', ul: '> ul', li: '> li' } });
 		lazyControl( ID );
 	}
 }
 
 function lazyControl( ID ){
-	if( ID.length > 0 ){
+	if( detectEl( ID ) ){
 		if( $('img.lazyload', ID).length > 0 )
 			$('img.lazyload', ID).unveil().trigger("unveil");
 	}
 }
 
 function iStylers(){	
-	if( $("select, input:checkbox, input:radio").length > 0 )
-		$("select, input:checkbox, input:radio").iStyler({wrapper:true});
+	if( detectEl( $("select, input:checkbox, input:radio") ) )
+		$("select, input:checkbox, input:radio").iStyler({ wrapper:true });
 }
 
 function changeDivPosition( type ){
