bed90316fee3d7bcb3515f0e231abf39079fca4b
burakkarakaya <burakkarakaya1984@gmail.com>
2019-03-15 08:53:52 +0300

    site bazlı .js
    - login

---
+++ custom.js
@@ -0,0 +1,47 @@
+/* mini login ve login.aspx */
+var login = {
+    el: {
+        tab: '.ems-tab.ems-tab-login',
+        tabNavLogin: '.ems-tab-header [rel="tab-1"]',
+        tabNavSignup: '.ems-tab-header [rel="tab-2"]',
+        errLogin: '[id$="lbfUYE_HATALIGIRIS"]',
+        errSignup: '.pageLogin_yeniUyelikHata'
+    },
+    cls: {
+        selected: 'selected'
+    },
+    initPlugins: function () {
+        var _t = this,
+            tab = $(_t.el.tab);
+        if (uty.detectEl(tab))
+            plugin.tabMenu.set(tab);
+    },
+    check: function () {
+        var _t = this;
+
+        if( uty.detectEl( $( _t.el.tab ) ) ){
+            /* 
+            url de ?signup=true geçiyor ve signup hata mesajı varsa signup selected classı atar böylece ilk açılışta signup seçili gelir */
+            var k = minusLoc.get('?', 'signup') || '';
+            if (k == 'true' || uty.detectEl($(_t.el.errSignup))) {
+                $(_t.el.errLogin).removeClass(_t.cls['selected']);
+                $(_t.el.tabNavSignup).addClass(_t.cls['selected']);
+            }
+        }    
+        
+    },
+    addEvent: function(){
+        
+    },
+    init: function () {
+        var _t = this;
+        _t.check();
+        _t.initPlugins();
+    }
+};
+
+login.init();
+
+function onUyeLogin() { modules.login.set(); }
+onUyeLogin();
+stage.addEventListener("CustomEvent", [{ type: "uyeLogin", handler: "onUyeLogin" }]);
